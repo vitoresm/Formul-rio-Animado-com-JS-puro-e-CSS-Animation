1834a5acd98970fe963e3708927add1226658c9d
vitoresm <vitormartins.vv48@gmail.com>
2020-03-21 22:36:22 -0300

    vibrar o formulário caso esteja vazio

--- base/script.js
+++ base/script.js
@@ -3,17 +3,46 @@ const form = document.querySelector("form");
 
 btnLogin.addEventListener("click", event => {
     event.preventDefault();
-    form.classList.add("form-hide");
-})
+   // form.classList.add("form-hide");
+
+    const fields = [...document.querySelectorAll(".input-block input")];
+
+    fields.forEach(field => {
+        if (field.value == "") form.classList.add("validate-error");
+    });
+
+    const formError = document.querySelector(".validate-error");
+
+    if (formError) {
+        formError.addEventListener("animationend", event => {
+            if (event.animationName === "nono") {
+                formError.classList.remove("validate-error");
+            
+            }
+        });
+
+    } else {
+        form.classList.add("form-hide");
+    }
+
+});
+
+
+    
+ 
 
 form.addEventListener("animationstart", event => {
-    if (event.animationName == "down"){
+    if (event.animationName === "down"){
         document.querySelector("body").style.overflow = "hidden";
     }
 });
 
-form.addEventListener("animationend", (event) => {
-    if (event.animationName == "down"){
-        form.style.display = "none";
-    }
-})
+    form.addEventListener("animationend", event => {
+        if (event.animationName === "down") {
+            form.style.display = "none";
+            document.querySelector("body").style.overflow = "none";
+        
+        }
+
+
+    })
